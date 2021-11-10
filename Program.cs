48ed15feaf72a3f5e6ec0cd0a849b921e903ab71
AfKsenia <afksenia@gmail.com>
2021-11-10 19:09:28 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите К");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine( "Введите b");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Equation equation = new Equation(k, b);
+            equation.Root();
+
+            Console.ReadKey();
+        }
+    }
+    public struct Equation
+    {
+        public Equation(double k, double b)
+        {
+            this.b = b;
+            this.k = k;
+
+        }
+        public double k;
+        public double b;
+        public void Root()
+        {
+            Console.WriteLine("X={0}", -b/k);
+        }
+
+    }
+}
